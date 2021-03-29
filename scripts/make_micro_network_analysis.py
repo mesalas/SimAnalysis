805ab8d482c7bcece5ebae519ad5bc253811ce35
Nicholas <nskg@pm.me>
2021-03-29 22:37:15 +0200

    Calculate volumes and relative volumes for edges and nodes using nx

--- scripts/make_micro_network_analysis.py
+++ scripts/make_micro_network_analysis.py
@@ -5,10 +5,23 @@ import community
 from collections import defaultdict
 from scripts.negopy import negopy,nx2df
 
+def calculate_total_node_volume(graph : nx.DiGraph):
+    for node in list(graph.nodes):
+        graph.nodes[node]["out volume"] = int(sum([graph.edges[e[0], e[1]]["weight"] for e in graph.out_edges(node)]))
+        graph.nodes[node]["in volume"] = int(sum([graph.edges[e[0], e[1]]["weight"] for e in graph.in_edges(node)]))
+        graph.nodes[node]["volume"] = graph.nodes[node]["out volume"] + graph.nodes[node]["in volume"]
+    return graph
+
 def normalize_directional_graph(graph):
-    adjacency_df = nx.to_pandas_adjacency(graph)
-    normalized_adjacency_df = adjacency_df.div(adjacency_df.sum(axis=1), axis=0).fillna(0)
-    return nx.from_pandas_adjacency(normalized_adjacency_df, create_using= nx.DiGraph)
+    for node in list(graph.nodes):
+        for out_edge in list(graph.out_edges(node)):
+            graph.edges[out_edge]["volume"] = graph.edges[out_edge]["weight"]
+            graph.edges[out_edge]["weight"] = graph.edges[out_edge]["weight"] / graph.nodes[node]["out volume"]
+
+
+    #adjacency_df = nx.to_pandas_adjacency(graph)
+    #normalized_adjacency_df = adjacency_df.div(adjacency_df.sum(axis=1), axis=0).fillna(0)
+    #return nx.from_pandas_adjacency(normalized_adjacency_df, create_using= nx.DiGraph)
 
 def make_micro_network_analysis(nodes, edges,graph):
 
@@ -28,8 +41,8 @@ def make_negopy_clustering_analysis(graph):
     return make_macro_network_analysis(graph, make_negopy_clusters)
 
 def make_macro_network_analysis(graph, clustering_method):
-    normalized_graph = normalize_directional_graph(graph)
-    part = clustering_method(nx.to_undirected(normalized_graph))
+    normalize_directional_graph(graph)
+    part = clustering_method(nx.to_undirected(graph))
 
     print("Number of communities {}".format(np.max([part[i] for i in part.keys()])))
     clique_agents_map = defaultdict(list)
@@ -40,7 +53,7 @@ def make_macro_network_analysis(graph, clustering_method):
         clique_agents_map[val].append(key)
 
     # Volume traded by clique
-    total_vol = np.sum([graph[edge[0]][edge[1]]["weight"] for edge in graph.edges()])
+    total_vol = np.sum([graph[edge[0]][edge[1]]["volume"] for edge in graph.edges()])
     print(total_vol)
     ave_vol = list()
     ave_clique_vol = list()
@@ -52,17 +65,16 @@ def make_macro_network_analysis(graph, clustering_method):
     for clique,agents in clique_agents_map.items():
         print("Clique no {}".format(clique))
 
-        clique_graph_unnormalized = graph.subgraph(agents) # make subgraph of clique with volumes
-        clique_graph_normalized = normalized_graph.subgraph(agents) # make subgraph of clique with normalized volumes
+        clique_graph = graph.subgraph(agents) # make subgraph of clique with volumes
 
         relative_vol_within_clique = list()
         vol = 0
 
 
         for agent in agents:
-            vol += np.sum([data["weight"] for data in graph[agent].values()]) #sum volume traded by agent to other agents
-            normalized_graph.nodes[agent]["community"] = clique #set the community property to the clique number
-            relative_vol_within_clique.append(np.sum([data["weight"] for data in clique_graph_normalized[agent].values()]))
+            vol += np.sum([data["volume"] for data in graph[agent].values()]) #sum volume traded by agent to other agents
+            graph.nodes[agent]["community"] = clique #set the community property to the clique number
+            relative_vol_within_clique.append(np.sum([data["weight"] for data in clique_graph[agent].values()]))
 
 
         pct_of_nodes = 100.*len(agents)/len(graph.nodes())
@@ -73,9 +85,9 @@ def make_macro_network_analysis(graph, clustering_method):
 
 
 
-        vol_within_clique = np.sum([clique_graph_unnormalized[edge[0]][edge[1]]["weight"] for edge in clique_graph_unnormalized.edges()])
+        vol_within_clique = np.sum([clique_graph[edge[0]][edge[1]]["volume"] for edge in clique_graph.edges()])
         print(vol_within_clique,vol_within_clique/total_vol)
-        density = nx.density(clique_graph_unnormalized)
+        density = nx.density(clique_graph)
 
         print("density {}".format(density))
         if pct_of_nodes >= 5.:
@@ -93,7 +105,7 @@ def make_macro_network_analysis(graph, clustering_method):
     print("average volume traded within clique: {}\n fraction of total {}".format(np.mean(ave_clique_vol),np.mean(ave_clique_vol)/total_vol))
     print("average fraction of traded volume from within clique {}".format(np.mean(ave_relative_vol_within_clique)))
     print("***\n")
-    return normalized_graph
+    return graph
 
 
 def make_micro_and_macro_network_analysis(input_path, cutoff,output_path):
@@ -103,9 +115,12 @@ def make_micro_and_macro_network_analysis(input_path, cutoff,output_path):
     graph.add_nodes_from(nodes)
     graph.add_weighted_edges_from(edges)
 
+    # Calculate the total volume traded by each node
+    calculate_total_node_volume(graph)
+
     make_micro_network_analysis(nodes, edges, graph)
-    #directed_graph_with_cliques = make_louvian_clustering_analysis(graph)
-    directed_graph_with_cliques = make_negopy_clustering_analysis(graph)
+    directed_graph_with_cliques = make_louvian_clustering_analysis(graph)
+    #directed_graph_with_cliques = make_negopy_clustering_analysis(graph)
     nx.write_gexf(directed_graph_with_cliques, output_path)
 
 
--- scripts/test_network_analysis.py
+++ scripts/test_network_analysis.py
@@ -2,7 +2,7 @@ import unittest
 import pandas as pd
 import networkx as nx
 import scripts.make_micro_network_analysis as make_micro_network_analysis
-from scripts.make_micro_network_analysis import normalize_directional_graph
+from scripts.make_micro_network_analysis import normalize_directional_graph,calculate_total_node_volume
 
 class MyTestCase(unittest.TestCase):
     def test_normalize_graph(self):
@@ -10,17 +10,23 @@ class MyTestCase(unittest.TestCase):
         graph = nx.from_pandas_adjacency(matrix, create_using= nx.DiGraph)
         print(matrix)
         print(nx.to_pandas_adjacency(graph))
-        print(nx.to_pandas_adjacency(normalize_directional_graph(graph)))
+        calculate_total_node_volume(graph)
+        normalize_directional_graph(graph)
+        print(nx.to_pandas_adjacency(graph))
 
 
 
     def test_resample_and_plot_data(self):
+
         inputs = ["testing/test_data/ABC_NYSE@0_Matching-MatchedOrders.csv",
                   "testing/test_data/DEF_NYSE@0_Matching-MatchedOrders.csv",
                   "testing/test_data/GHI_NYSE@0_Matching-MatchedOrders.csv"]
-        outputs = ["testing/test_data/reduced_data/0_ABC_graph_cliques_05.gexf",
-                   "testing/test_data/reduced_data/0_DEF_graph_cliques_05.gexf",
-                   "testing/test_data/reduced_data/0_GHI_graph_cliques_05.gexf"]
+        inputs = ["../Simulations/run92/batch11/wd/ABC_NYSE@11_Matching-MatchedOrders.csv"]#,
+                  #"../Simulations/run87/batch1/working/DEF_NYSE@0_Matching-MatchedOrders.csv",
+                  #"../Simulations/run87/batch1/working/GHI_NYSE@0_Matching-MatchedOrders.csv"]
+        outputs = ["testing/test_data/reduced_data/92_11_0_ABC_graph_cliques_05.gexf"]#,
+                   #"testing/test_data/reduced_data/0_DEF_graph_cliques_05.gexf",
+                   #"testing/test_data/reduced_data/0_GHI_graph_cliques_05.gexf"]
 
         [make_micro_network_analysis.make_micro_and_macro_network_analysis(input_file, 0.05,output_file) for input_file,output_file in zip(inputs,outputs)]
 
