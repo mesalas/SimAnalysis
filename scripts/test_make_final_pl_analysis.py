7b001d20d655ae57a93374f3eb5f3a1217893b2d
Nicholas <nskg@pm.me>
2021-04-08 11:03:10 +0200

    run94

--- scripts/test_make_final_pl_analysis.py
+++ scripts/test_make_final_pl_analysis.py
@@ -8,49 +8,49 @@ import plot_daily_pl as plot_daily_pl
 
 class MyTestCase(unittest.TestCase):
     def test_pl_analysis(self):
-        dispersed_inputs = [["../temp_data/run93/batch1/working/reduced_data/1_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch1/working/reduced_data/1_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch1/working/reduced_data/1_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch2/working/reduced_data/2_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch2/working/reduced_data/2_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch2/working/reduced_data/2_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch3/working/reduced_data/3_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch3/working/reduced_data/3_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch3/working/reduced_data/3_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch4/working/reduced_data/4_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch4/working/reduced_data/4_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch4/working/reduced_data/4_GHI_agent_daily_volume.csv.gz"]
+        dispersed_inputs = [["../temp_data/run94/batch1/working/reduced_data/1_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch1/working/reduced_data/1_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch1/working/reduced_data/1_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch2/working/reduced_data/2_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch2/working/reduced_data/2_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch2/working/reduced_data/2_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch3/working/reduced_data/3_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch3/working/reduced_data/3_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch3/working/reduced_data/3_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch4/working/reduced_data/4_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch4/working/reduced_data/4_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch4/working/reduced_data/4_GHI_agent_daily_volume.csv.gz"]
                   ]
 
-        intermediate_inputs = [["../temp_data/run93/batch5/working/reduced_data/5_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch5/working/reduced_data/5_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch5/working/reduced_data/5_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch6/working/reduced_data/6_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch6/working/reduced_data/6_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch6/working/reduced_data/6_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch7/working/reduced_data/7_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch7/working/reduced_data/7_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch7/working/reduced_data/7_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch8/working/reduced_data/8_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch8/working/reduced_data/8_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch8/working/reduced_data/8_GHI_agent_daily_volume.csv.gz"]
+        intermediate_inputs = [["../temp_data/run94/batch5/working/reduced_data/5_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch5/working/reduced_data/5_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch5/working/reduced_data/5_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch6/working/reduced_data/6_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch6/working/reduced_data/6_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch6/working/reduced_data/6_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch7/working/reduced_data/7_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch7/working/reduced_data/7_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch7/working/reduced_data/7_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch8/working/reduced_data/8_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch8/working/reduced_data/8_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch8/working/reduced_data/8_GHI_agent_daily_volume.csv.gz"]
                   ]
-        concentrated_inputs = [["../temp_data/run93/batch10/working/reduced_data/10_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch10/working/reduced_data/10_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch10/working/reduced_data/10_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch11/working/reduced_data/11_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch11/working/reduced_data/11_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch11/working/reduced_data/11_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch12/working/reduced_data/12_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch12/working/reduced_data/12_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch12/working/reduced_data/12_GHI_agent_daily_volume.csv.gz"],
-                  ["../temp_data/run93/batch9/working/reduced_data/9_ABC_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch9/working/reduced_data/9_DEF_agent_daily_volume.csv.gz",
-                   "../temp_data/run93/batch9/working/reduced_data/9_GHI_agent_daily_volume.csv.gz"]
+        concentrated_inputs = [["../temp_data/run94/batch10/working/reduced_data/10_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch10/working/reduced_data/10_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch10/working/reduced_data/10_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch11/working/reduced_data/11_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch11/working/reduced_data/11_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch11/working/reduced_data/11_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch12/working/reduced_data/12_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch12/working/reduced_data/12_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch12/working/reduced_data/12_GHI_agent_daily_volume.csv.gz"],
+                  ["../temp_data/run94/batch9/working/reduced_data/9_ABC_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch9/working/reduced_data/9_DEF_agent_daily_volume.csv.gz",
+                   "../temp_data/run94/batch9/working/reduced_data/9_GHI_agent_daily_volume.csv.gz"]
                   ]
 
 
-        output = "../temp_data/run93/final_volume.png"
+        output = "../temp_data/run94/final_volume.png"
 
         make_final_pl_analysis.make_final_pl_analysis(dispersed_inputs,intermediate_inputs,concentrated_inputs, output)
 
--- scripts/test_make_summary_of_full_networks.py
+++ scripts/test_make_summary_of_full_networks.py
@@ -15,14 +15,14 @@ class MyTestCase(unittest.TestCase):
     def test_pl_analysis(self):
         hm_fig,hm_ax = plt.subplots(nrows=2, ncols=3, figsize = (20,10))
         fig,ax = plt.subplots(nrows=2, ncols=3, figsize = (20,10))
-        dispersed_inputs = [pd.read_csv("../temp_data/run93/batch{}/working/reduced_data/{}_ABC_directed_graph_sum.csv".format(num,num)) for num in range(1,5)]
+        dispersed_inputs = [pd.read_csv("../temp_data/run94/batch{}/working/reduced_data/{}_ABC_directed_graph_sum.csv".format(num,num)) for num in range(1,5)]
 
-        pd.concat(dispersed_inputs).groupby(level=0).mean().to_csv("../temp_data/run93/dispersed_full_net_stats.csv")
+        pd.concat(dispersed_inputs).groupby(level=0).mean().to_csv("../temp_data/run94/dispersed_full_net_stats.csv")
 
-        intermediate_inputs = [pd.read_csv("../temp_data/run93/batch{}/working/reduced_data/{}_ABC_directed_graph_sum.csv".format(num,num)) for num in range(5,9)]
-        pd.concat(intermediate_inputs).groupby(level=0).mean().to_csv("../temp_data/run93/intermediate_full_net_stats.csv")
-        concentrated_inputs = [pd.read_csv("../temp_data/run93/batch{}/working/reduced_data/{}_ABC_directed_graph_sum.csv".format(num,num)) for num in range(9,13)]
-        pd.concat(concentrated_inputs).groupby(level=0).mean().to_csv("../temp_data/run93/concentrated_full_net_stats.csv")
+        intermediate_inputs = [pd.read_csv("../temp_data/run94/batch{}/working/reduced_data/{}_ABC_directed_graph_sum.csv".format(num,num)) for num in range(5,9)]
+        pd.concat(intermediate_inputs).groupby(level=0).mean().to_csv("../temp_data/run94/intermediate_full_net_stats.csv")
+        concentrated_inputs = [pd.read_csv("../temp_data/run94/batch{}/working/reduced_data/{}_ABC_directed_graph_sum.csv".format(num,num)) for num in range(9,13)]
+        pd.concat(concentrated_inputs).groupby(level=0).mean().to_csv("../temp_data/run94/concentrated_full_net_stats.csv")
 
 if __name__ == '__main__':
     unittest.main()
